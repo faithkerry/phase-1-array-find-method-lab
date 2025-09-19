f6ec54a898a2e67ba29be75fb6c58e8c49ed153a
faith-kerry <kerubofaith944@gmail.com>
2025-09-19 20:56:48 +0300

    worked on the array find method

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+  // Find the first object where the result is "W"
+  const win = record.find(game => game.result === "W");
+  
+  // If found, return the year; otherwise undefined
+  return win ? win.year : undefined;
+}
+
+module.exports = {
+  superbowlWin
+};
+
